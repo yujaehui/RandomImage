bb12e333f39a0873a2435cc70a78e4f38d3b4f9d
Jaehui Yu <pkj0522@naver.com>
2024-04-21 18:47:22 +0900

    Initial Commit

---
+++ RandomImage/RandomImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomImageApp.swift
+//  RandomImage
+//
+//  Created by Jaehui Yu on 4/21/24.
+//
+
+import SwiftUI
+
+@main
+struct RandomImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
